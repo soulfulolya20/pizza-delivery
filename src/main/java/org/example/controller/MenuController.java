bb45c62c849604b57f386ab719dfaed9572765ff
soulfulolya20 <soulful.olya@gmail.com>
2022-12-08 15:36:43 +0300

    убрала ненужные файлы

--- src/main/java/org/example/controller/MenuController.java
+++
@@ -1,9 +0,0 @@
-package org.example.controller;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping(value = "/menu")
-public class MenuController {
-}
